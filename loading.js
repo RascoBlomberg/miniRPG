a0eba4e6a6c7dc7b41c3ebe866a1f56e92f25e06
Oscar <Oscar.Blomberg1@medieinstitutet.se>
2026-01-03 14:22:49 +0100

    koden ligger i ny JS filer

---
+++ loading.js
@@ -0,0 +1,23 @@
+const getPage = new URLSearchParams(window.location.search);
+
+const fromPage = getPage.get("from");
+const toPage = getPage.get("to");
+
+console.log(fromPage);
+console.log(toPage);
+
+setTimeout(function() {
+const ref = document.referrer;
+
+if (fromPage === "startpage" && toPage === "shop") {
+    window.location.href = "shop.html";
+    } 
+else if (fromPage === "startpage" && toPage === "farm") {
+    window.location.href = "farm.html";
+    } 
+else {
+    window.location.href = "index.html";
+    console.log("fel")
+    }
+
+}, 2000);
