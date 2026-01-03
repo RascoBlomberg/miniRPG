0cec60e5b073960f085656fb3b47f54203388664
Oscar <Oscar.Blomberg1@medieinstitutet.se>
2026-01-03 14:20:54 +0100

    Flyttade JS till extern fil

---
+++ farm.js
@@ -0,0 +1,177 @@
+window.addEventListener("DOMContentLoaded", LSloadProgress);
+
+let moneyValue = 20;
+
+const btn = document.getElementById("btn");
+const btn1 = document.getElementById("btn1");
+const box1 = document.querySelector(".box1");
+const box2 = document.querySelector(".box2");
+const box3 = document.querySelector(".box3");
+const box4 = document.querySelector(".box4");
+const box5 = document.querySelector(".box5");
+const box6 = document.querySelector(".box6");
+const box7 = document.querySelector(".box7");
+
+document.getElementById("myBox").textContent = moneyValue;
+/*
+btn.addEventListener("click", () => {
+    box.classList.add("expanded");
+    box1.classList.add("expanded");
+    box2.classList.add("expanded");
+    box3.classList.add("expanded");
+}); */
+
+//varje planta borde kosta 2mV för att växa
+
+const colorCycle = [
+"#FFFF66",
+"#CCFF66",
+"#99FF66",
+"#66FF66",
+"#33CC33",
+];
+
+let plantGrowNumb = [];
+const nightTimeCycle= [
+     "assets/sol2.png", "assets/sol3.png", "assets/moon1.png", "assets/moon2.png", "assets/moon3.png", "assets/sol1.png",
+];
+
+nightTimeCycle.forEach(src => {
+    const img = new Image();
+    img.src = src;
+});
+
+function growPlant(){
+    if (moneyValue >= 20){
+        for (let i = 0; i < moneyValue; i++) {
+            if (!plantGrowNumb[i]) plantGrowNumb[i] = [];
+            plantGrowNumb[i].push("box"+i)
+            if (i == 9){
+                moneyValue = moneyValue - (i*2);
+                break;
+            }
+        }
+        
+        plantGrowNumb.forEach(boxArray => {
+            boxArray.forEach(boxClass => {
+                const box = document.querySelector("." + boxClass);
+                box?.classList.add("expanded");
+            });
+        });
+
+    for (let i = 0; i <= 5; i++) {
+        setTimeout(() => {
+            document.body.style.backgroundImage = `url("${nightTimeCycle[i]}")`;
+            plantGrowNumb.forEach(boxArray => {
+                boxArray.forEach(boxClass => {
+                    const box = document.querySelector("." + boxClass + ".expanded"); // target: boxN med expanded
+                    if (box) {
+                        box.style.backgroundColor = colorCycle[i];
+                    }
+                });
+            }); 
+        }, 1000 * i);
+        if (i == 6) document.body.style.backgroundImage = 'url("assets/sol1.png")';
+    }
+    
+
+    document.getElementById("myBox").textContent = moneyValue;      
+    } else {
+        alert("inte tillräckligt med pengar");
+    }
+}
+
+function MoneyUp() {
+    moneyValue++;
+    document.getElementById("myBox").textContent = moneyValue;
+    return moneyValue;
+}
+
+/*
+function setupBox(box) {
+    let lastMoveTime = 0;
+
+    document.addEventListener("mousedown", () => {
+        const now = performance.now();
+        box.addEventListener("mousemove", () => {
+            lastMoveTime = performance.now();
+                const now = performance.now();
+            if (now - lastMoveTime <= 100) {
+                console.log("mousemove hände nyligen!");
+                box.classList.remove("expanded");
+            } else {
+                console.log("mousemove var FÖR LÄNGE sedan.");
+            }
+        })
+    });
+
+}
+
+*/
+
+
+
+let isMouseDown = false;
+
+
+document.addEventListener("mousedown", () => {
+    isMouseDown = true;
+});
+
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+});
+
+
+function setupBox(box) {
+    let lastMoveTime = 0;
+
+    
+    box.addEventListener("mousemove", () => {
+        lastMoveTime = performance.now();
+
+        
+        if (isMouseDown) {
+            const now = performance.now();
+            if (now - lastMoveTime <= 100) {
+                console.log(`mousemove + mousedown på ${box.className}`);
+                if (box.classList.length > 1) {moneyValue += 5;}
+                box.classList.remove("expanded");
+                box.style.backgroundColor = "yellow";
+                document.getElementById("myBox").textContent = moneyValue;
+                return moneyValue;
+            }
+        }
+    });
+
+   
+
+}
+
+
+const boxes = document.querySelectorAll(".box1, .box2, .box3, .box4, .box5, .box6, .box7, .box8");
+
+boxes.forEach(box => setupBox(box));
+
+document.body.style.backgroundImage = 'url("assets/sol1.png")';
+
+// progress element 
+// 0 - moneyValue
+// 1 - start turtorial panel
+// 2 - shop turtorial panel 1
+// 3 - farm turtorial panel
+// 4 - shop turtorial panel 2
+
+
+
+function returnHome() {
+    progress[0] = moneyValue;
+    localStorage.progress = JSON.stringify(progress);
+
+    window.location.href = "loading.html";
+}
+
+function LSloadProgress() {
+    if (!localStorage.progress) return;
+    progress = JSON.parse(localStorage.progress);
+}
